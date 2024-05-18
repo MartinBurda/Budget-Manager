46f90c8ee641ef9443d6c338dbf00943a968b1be
MarekTrijanOSSP <marek.trojan@student.ossp.cz>
2024-05-19 01:54:34 +0200

    commit

--- app/frontend/index.py
+++ app/frontend/index.py
@@ -3,6 +3,15 @@ from tkinter import *
 import tkinter as tk
 
 
+def robrazeni():
+    puvodni_text = vloste_castku.get()
+    pridani_castky = sifra(pridani_castky, "castka")
+    label_result.config(text="Pridana castka: " + pridani_castky)
+    
+def desifrovat_text():
+    sifrovani_text = vloste_vydaje.get()
+    pridani_vydaju = sifra(pridani_vydaju, "vydaje")
+    label_result.config(text="Pridane vydaje: " + pridani_vydaju)
 
 
 root = tk.Tk()
@@ -33,5 +42,8 @@ encrypt_btn.pack(side='left', padx=5)
 decrypt_btn = Button(root, text="desifrovat", command=pridani_vydaju)
 decrypt_btn.pack(side='right', padx=5)
 
+
+label_result = tk.Label(root, text="")
+label_result.pack()
 
 root.mainloop()
